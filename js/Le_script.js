842da91e6c1d9c42fece10352f6d901c81d35d5b
Park6800@naver.com <wnsqja6800@naver.com>
2024-03-15 22:36:44 +0900

    Fix

--- js/Le_script.js
+++ js/Le_script.js
@@ -70,7 +70,6 @@ buttonLeft.addEventListener('click', () => {
     // 너비 비례 slidesPerView가 많으면 루프 작동 안함
     loop: true,
     
-
     autoplay: {     //자동슬라이드 (false-비활성화)
       delay: 4000, // 시간 설정
       disableOnInteraction: true, // false-스와이프 후 자동 재생
@@ -105,6 +104,6 @@ buttonLeft.addEventListener('click', () => {
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
-    },
+    },	
     // And if we need scrollbar
   });
